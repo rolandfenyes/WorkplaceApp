4e11a08ac4a542b8555118ca039dded0f3563f34
rolandfenyes <ftendalan@gmail.com>
2020-05-07 11:33:07 +0200

    Activity holiday xml is done

--- app/src/main/java/aut/bme/hu/workplaceapp/data/DataManager.kt
+++ app/src/main/java/aut/bme/hu/workplaceapp/data/DataManager.kt
@@ -1,6 +1,12 @@
 package aut.bme.hu.workplaceapp.data
 
 object DataManager {
+
+    private const val HOLIDAY_MAX_VALUE = 20
+    private const val HOLIDAY_DEFAULT_VALUE = 15
+
+    val holidays = HOLIDAY_DEFAULT_VALUE
+
     val person: Person = Person(
         "Test User",
         "testuser@domain.com",
@@ -10,4 +16,6 @@ object DataManager {
         "1234567890",
         "012345678"
     )
+
+    fun getRemainingHolidays(): Int = HOLIDAY_MAX_VALUE - holidays
 }
