87c79f7dd101455bb0579fd0ee37de9defcfdc6a
rolandfenyes <ftendalan@gmail.com>
2020-05-07 12:03:30 +0200

    chart added to holiday activity

--- app/src/main/java/aut/bme/hu/workplaceapp/HolidayActivity.kt
+++ app/src/main/java/aut/bme/hu/workplaceapp/HolidayActivity.kt
@@ -2,11 +2,41 @@ package aut.bme.hu.workplaceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import aut.bme.hu.workplaceapp.data.DataManager
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.utils.ColorTemplate
+import kotlinx.android.synthetic.main.activity_holiday.*
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class HolidayActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_holiday)
+
+        btnTakeHoliday.setOnClickListener {
+            //TODO
+        }
+
+        loadHolidays()
+
     }
+
+    private fun loadHolidays() {
+        var entries: ArrayList<PieEntry> = ArrayList()
+
+        entries.add(PieEntry(DataManager.holidays.toFloat(), "Taken"))
+        entries.add(PieEntry(DataManager.getRemainingHolidays().toFloat(), "Remaining"))
+
+        val dataSet = PieDataSet(entries, "Holidays")
+        dataSet.setColors(*ColorTemplate.MATERIAL_COLORS)
+
+        val data = PieData(dataSet)
+        chartHoliday.data = data
+        chartHoliday.invalidate()
+    }
+
+
 }
