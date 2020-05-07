1ec7928835d840a137b28aadc01a7c359dbe1276
rolandfenyes <ftendalan@gmail.com>
2020-05-07 12:56:33 +0200

    Date validation and error message added

--- app/src/main/java/aut/bme/hu/workplaceapp/HolidayActivity.kt
+++ app/src/main/java/aut/bme/hu/workplaceapp/HolidayActivity.kt
@@ -3,6 +3,7 @@ package aut.bme.hu.workplaceapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.DatePicker
+import androidx.appcompat.app.AlertDialog
 import aut.bme.hu.workplaceapp.data.DataManager
 import aut.bme.hu.workplaceapp.fragments.DatePickerDialogFragment
 import com.github.mikephil.charting.data.PieData
@@ -45,11 +46,33 @@ class HolidayActivity : AppCompatActivity(), DatePickerDialogFragment.OnDateSele
     override fun onDateSelected(year: Int, month: Int, dayOfMonth: Int) {
         var numHolidays = DataManager.holidays
 
-        if (DataManager.getRemainingHolidays() > 0) {
+        if (DataManager.getRemainingHolidays() > 0 && dateValidation(year, month, dayOfMonth)) {
             DataManager.holidays = numHolidays + 1
+        } else {
+            createErrorMessage()
         }
 
         loadHolidays()
+    }
+
+    fun createErrorMessage() {
+        val builder = AlertDialog.Builder(this@HolidayActivity)
+        builder.setTitle("Error")
+        builder.setMessage("You cannot take holiday in the past!")
+        builder.setPositiveButton("OK") {dialog, which -> }
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    fun dateValidation(year: Int, month: Int, dayOfMonth: Int) : Boolean {
+        val current = Calendar.getInstance()
+        var currentYear = current.get(Calendar.YEAR)
+        var currentMonth = current.get(Calendar.MONTH)
+        var currentDay = current.get(Calendar.DAY_OF_MONTH)
+
+        if (year >= currentYear && month > currentMonth) {
+            return true
+        } else return year >= currentYear && month == currentMonth && dayOfMonth >= currentDay
     }
 
 
